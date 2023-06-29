94ab509aa3245bcdae46d3012ab66e1938d7ed1d
Игорь Байдерин <ibaid@mail.ru>
2023-06-29 11:38:20 +0500

    reformat date

--- js/test.js
+++ js/test.js
@@ -48,7 +48,7 @@ function getDateFromString(dateTime) {
     let year = objectDate.getFullYear();
     if (day < 10) day = '0' + day
     if (month < 10) month = `0${month}`
-    return `${day}-${month}-${year}`;
+    return `${year}-${month}-${day}`;
 }
 
 function setAutorAndTool() {
